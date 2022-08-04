85e76d67df9ea82035939c88f926a3c66ff0ba0d
Jesko Usinger <oksej.10@googlemail.com>
2022-08-04 16:29:55 +0200

    fixes

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -30,19 +30,19 @@ export default function ResultModal({
       onSubmit={handleSubmit}
       className="z-50 absolute top-1/2 bottom-1/2 -translate-y-1/2 flex flex-col justify-around items-center bg-white rounded-xl py-4 px-8 h-80 w-4/5 max-w-sm"
     >
-      <div className="w-full flex justify-between">
-        <span className="-rotate-12 text-xl text-red-500 font-medium">
+      <div className="w-full flex justify-center">
+        {/* <span className="-rotate-12 text-xl text-red-500 font-medium">
           {result ? result.fname : ''}
-        </span>
-        <p className="hidden md:block uppercase text-base font-semiBold  ">
+        </span> */}
+        <p className="uppercase text-base font-semiBold  ">
           {result ? result.percentage : '-'}/100
         </p>
-        <span className="rotate-12 text-xl text-red-500 font-medium">
+        {/* <span className="rotate-12 text-xl text-red-500 font-medium">
           {result ? result.sname : ''}
-        </span>
+        </span> */}
       </div>
       <div className="flex flex-col items-center justify-center">
-        <div className="w-20 h-20 bg-blue-500"></div>
+        <div className="w-20 h-20 border border-blue-500"></div>
         <p className="pt-6 uppercase text-base font-medium text-center  ">
           {result ? result.result : 'Kein Ergebnis'}
         </p>
